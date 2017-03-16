feaf2c3bb8af50ceae4ef460b916d99761fa040c
mokv <crispy9889@gmail.com>
2017-03-16 23:25:14 +0200

    JustImplemented

---
+++ Scripts/PlatformDestroyer.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+	public GameObject platformDestructionPoint;
+	private int obstacleOrNot;
+	private int obstacleOrNotRange;
+	private bool obstacleGoUp;
+	private bool obstacleGoDown;
+	private float obstacleMovement;
+	private float minHeight;
+	private GameObject maxHeightPoint;
+	private float maxHeight;
+	private int upOrDown;
+	private GameObject platformGenerator;
+
+	// Use this for initialization
+	void Start () {
+		platformDestructionPoint = GameObject.Find ("PlatformDestructionPoint");
+		platformGenerator = GameObject.Find ("PlatformGenerator");
+		obstacleMovement = platformGenerator.transform.position.y;
+		minHeight = transform.position.y;
+		maxHeightPoint = GameObject.Find ("MaxHeightPoint");
+		maxHeight = maxHeightPoint.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < platformDestructionPoint.transform.position.x) 
+		{
+			gameObject.SetActive(false);
+		}
+		if(obstacleOrNot == 1){
+  			if (obstacleGoDown) 
+  			{
+  				obstacleMovement -= Time.deltaTime*2;
+				gameObject.transform.position = new Vector3 (gameObject.transform.position.x, obstacleMovement, gameObject.transform.position.z);
+  				if (obstacleMovement <= minHeight) {
+  					obstacleGoUp = true;
+  					obstacleGoDown = false;
+  				}
+  			} 
+  			else if (obstacleGoUp) 
+  			{
+  				obstacleMovement += Time.deltaTime*2;
+				gameObject.transform.position = new Vector3 (gameObject.transform.position.x, obstacleMovement, gameObject.transform.position.z);
+  				if (obstacleMovement >= maxHeight) {
+  					obstacleGoDown = true;
+  					obstacleGoUp = false;
+  				}
+  			}
+
+
+
+		}
+	}
+
+	void ObstacleCheck(int changeObstacleOccurance){
+		obstacleOrNotRange = changeObstacleOccurance;
+		obstacleOrNot = Random.Range (1, obstacleOrNotRange);
+		//obstacleOrNot = 1;
+		upOrDown = Random.Range (0, 1);
+		if (upOrDown == 1) {
+			obstacleGoDown = true;
+		} else {
+			obstacleGoUp = true;
+		}
+	}
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private float moveSpeedStore;
+	public float jumpForce;
+	public float speedMultiplier;
+	public float speedIncreaseMilestone;
+	private float speedMilestoneCount;
+	private float speedMilestoneCountStore;
+	private float speedIncreaseMilestoneStore;
+
+	public float jumpTime;
+	private float jumpTimeCounter;
+
+	private Rigidbody2D myRigidbody;
+
+	public bool grounded;
+	public LayerMask whatIsGround;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public AudioSource deathSound;
+	public PhysicsMaterial2D bouncinessMaterialPlayer;
+	public float bouncingStart;
+	private float pointsPerSecondStore;
+
+	public bool canJump;
+	public bool doubleJump;
+
+	public GameManager theGameManager;
+	private ScoreManager theScoreManager;
+	private PlatformGenerator thePlatformGenerator;
+	private float scaleChangeMinStore;
+	private float scaleChangeMaxStore;
+	public float scoreIncreaserPerMilesetone;
+
+	// Use this for initialization
+	void Start () {
+		theScoreManager = FindObjectOfType<ScoreManager> ();
+		pointsPerSecondStore = theScoreManager.pointsPerSecond;
+		thePlatformGenerator = FindObjectOfType<PlatformGenerator> ();
+		scaleChangeMinStore = thePlatformGenerator.scaleChangeMin;
+		scaleChangeMaxStore = thePlatformGenerator.scaleChangeMax;
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+		jumpTimeCounter = jumpTime;
+		speedMilestoneCount = speedIncreaseMilestone;
+
+		moveSpeedStore = moveSpeed;
+		speedMilestoneCountStore = speedMilestoneCount;
+		speedIncreaseMilestoneStore = speedIncreaseMilestone;
+		bouncinessMaterialPlayer.bounciness = bouncingStart;
+		GetComponent<CircleCollider2D> ().enabled = false;
+		GetComponent<CircleCollider2D> ().enabled = true;
+		doubleJump = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+		if (transform.position.x > speedMilestoneCount) {
+			speedMilestoneCount += speedIncreaseMilestone;
+			speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;
+			moveSpeed = moveSpeed * speedMultiplier;
+			theScoreManager.pointsPerSecond *= scoreIncreaserPerMilesetone;
+			if (thePlatformGenerator.scaleChangeMin > 0.55f) {
+				thePlatformGenerator.scaleChangeMin -= thePlatformGenerator.scaleChangeReducer;
+				thePlatformGenerator.scaleChangeMax -= thePlatformGenerator.scaleChangeReducer;
+			}
+			if (thePlatformGenerator.obstacleOccurance > 1) {
+				thePlatformGenerator.obstacleOccurance--;
+			}
+			thePlatformGenerator.wallObstacleBool = true;
+			if(thePlatformGenerator.wallObstacleChanceRange > 5 ){
+				thePlatformGenerator.wallObstacleChanceRange--;
+			}
+		}
+
+		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			if (canJump) 
+			{
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+				canJump = false;
+				doubleJump = true;
+			}
+			else if (doubleJump) 
+			{
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+				doubleJump = false;
+			}
+		}
+
+		if (Input.GetMouseButton (0)) {
+			if (jumpTimeCounter > 0) {
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+				jumpTimeCounter -= Time.deltaTime;
+			}
+		}
+
+		if(Input.GetMouseButtonUp(0)){
+			if (doubleJump) {
+				jumpTimeCounter = jumpTime;
+			} else {
+				jumpTimeCounter = 0;
+			}
+		}
+		if (grounded) 
+		{
+			jumpTimeCounter = jumpTime;
+			canJump = true;
+		}
+			
+	}
+
+	void OnCollisionEnter2D ( Collision2D other)
+	{
+		if (other.gameObject.tag == "killbox") 
+		{
+			deathSound.Play ();
+			theGameManager.RestartGame ();
+			moveSpeed = moveSpeedStore;
+			speedMilestoneCount = speedMilestoneCountStore;
+			speedIncreaseMilestone = speedIncreaseMilestoneStore;
+			theScoreManager.pointsPerSecond = pointsPerSecondStore;
+			thePlatformGenerator.scaleChangeMax = scaleChangeMaxStore;
+			thePlatformGenerator.scaleChangeMin = scaleChangeMinStore;
+			thePlatformGenerator.obstacleOccurance = 9;
+			thePlatformGenerator.wallObstacleBool = false;
+			thePlatformGenerator.wallObstacleChanceRange = 19;
+		}
+	}
+}
